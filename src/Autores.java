874d1a81b7e25bab91482e3c842a9c3e9c3940f5
markel <markel.martin06@somo.eus>
2026-02-05 09:24:22 +0100

    Estado_inicial

---
+++ src/Autores.java
@@ -0,0 +1,56 @@
+public class Autores {
+/* Atributos de la clase autores */
+    private String nombre;
+    private String nacionalidad;
+    private int edad;
+    private int numLibros;
+    private int cod_autor;
+/* Metodo constructor de la clase autores */
+    public Autores(String nombre, String nacionalidad, int edad, int numLibros, int cod_autor){
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.edad = edad;
+        this.numLibros = numLibros;
+        this.cod_autor = cod_autor;
+    }
+/* Metodos getters de la clase autores */
+    public String getNombre() {
+        return nombre;
+    }
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public int getNumLibros() {
+        return numLibros;
+    }
+    public int getCod_autor() {
+        return cod_autor;
+    }
+/* Metodos setters de la clase autores */
+    public void setNumLibros(int numLibros) {
+        if (numLibros >= 0)
+            this.numLibros = numLibros;
+    }
+    public void setEdad(int edad) {
+        if (edad > 0)
+            this.edad = edad;
+    }
+/*hash y equals */
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(cod_autor);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Autores autor = (Autores) obj;
+        return cod_autor == autor.cod_autor;
+    }
+
+}
+
---
+++ src/Libros.java
@@ -0,0 +1,61 @@
+public class Libros {
+    /* Atributos de la clase libros */
+    private String titulo;
+    private Autores Autores;
+    private int anioPublic;
+    private String genero;
+    private double precio;
+    private int paginas;
+    private int cod_libro;
+/* Metodo constructor de la clase libros */
+    public Libros(String titulo, Autores Autores, int anioPublic, String genero, double precio, int paginas) {
+        this.titulo = titulo;
+        this.Autores = Autores;
+        this.anioPublic = anioPublic;
+        this.genero = genero;
+        this.precio = precio;
+        this.paginas = paginas;
+        this.cod_libro = (int) (Math.random() * 100000);
+    }
+/* Metodos getters de la clase libros */
+    public String getTitulo() {
+        return titulo;
+    }
+    public Autores getAutores() {
+        return Autores;
+    }
+    public int getAnioPublic() {
+        return anioPublic;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public int getPaginas() {
+        return paginas;
+    }
+    public int getCod_libro() {
+        return cod_libro;
+    }
+
+/* Metodo setters de la clase libros */
+    public void setPrecio(double precio) {
+        if (precio >= 0)
+            this.precio = precio;
+    }
+/*Equals y hash*/
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(cod_libro);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Libros libro = (Libros) obj;
+        return cod_libro == libro.cod_libro;
+    }
+}
